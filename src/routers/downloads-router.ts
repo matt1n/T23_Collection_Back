123e6e0de8805d41385e5b32706fa7d0688ffe4e
Mateus Macêdo <mateusamacedo5@gmail.com>
2023-09-12 15:12:23 -0300

    feat: new protocols

--- src/routers/downloads-router.ts
+++ src/routers/downloads-router.ts
@@ -6,8 +6,18 @@ downloadRouter
 .get("/day1", (req, res) => {
     res.download("public/dnd.pdf")
 })
-.get("/day5", (req, res) => {
-    res.download("public/Protocolo29-08.pdf")
+.get("/day3", (req, res) => {
+    res.download("public/Protocolo3.pdf")
 })
+.get("/day5", (req, res) => {
+    res.download("public/Protocolo5.pdf")
+})
+.get("/day6", (req, res) => {
+    res.download("public/Protocolo6.pdf")
+})
+.get("/day7", (req, res) => {
+    res.download("public/Protocolo7.pdf")
+})
+
 
 export {downloadRouter}
